c0f168a9d4198f2f9068fa9c9e6881dc9bddbc20
Abdul Wahid <wahdiahmed890@gmail.com>
2021-06-22 17:54:07 +0600

    Table commit

--- server-site/database/migrations/2021_06_22_111341_cartegory_list_table.php
+++ server-site/database/migrations/2021_06_22_111341_cartegory_list_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CartegoryListTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('category_list',function (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('cat_name',200);
+            $table->string('cat_code',200);
+            $table->string('cat_icon',200);
         });
+
     }
 
     /**
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 }
